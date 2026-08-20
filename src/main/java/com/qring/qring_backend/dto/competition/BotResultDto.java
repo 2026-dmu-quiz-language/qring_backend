2a4161be2b29553040b3ec46549a9c9e2ef5d74f
aprilily <chae042740@naver.com>
2026-08-20 19:02:08 +0900

    feat: 스트릭 보너스 로직 옵션 B로 변경 + 보너스 값 조정

--- src/main/java/com/qring/qring_backend/dto/competition/BotResultDto.java
+++ src/main/java/com/qring/qring_backend/dto/competition/BotResultDto.java
@@ -9,24 +9,19 @@ import lombok.Setter;
 
 public class BotResultDto {
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
+    @Getter @Setter @NoArgsConstructor
     public static class AnswerItem {
-        private String sourceType; // STORY / COMPETITION
+        private String sourceType;        // STORY / COMPETITION
         private Long sourceQuizContentId;
         private Integer roundNo;
         private String userAnswer;
         private boolean userIsCorrect;
-        private boolean botIsCorrect; // /bot/level 응답에서 받은 값 그대로 echo
+        private boolean botIsCorrect;     // /bot/level 응답에서 받은 값 그대로 echo
     }
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
+    @Getter @Setter @NoArgsConstructor
     public static class Request {
-        private List<AnswerItem> answers; // 21개 (맞은 문제 + 틀린 문제 전부)
-        private Integer streakCount; // 연속 정답 횟수 (최대치)
+        private List<AnswerItem> answers;   // 21개 (맞은 문제 + 틀린 문제 전부, roundNo 순서 무관하게 보내도 서버가 정렬함)
     }
 
     @Getter
